c041d9c0a0757b877bb983797c8d6799c50b8f3a
Massimiliano Bocchieri (aka Max) <massimiliano.bocchieri@gmail.com>
2020-12-20 23:49:56 +0100

    add travis, some dependencies and plugins

---
+++ src/test/java/io/bocchieri/codingexperience/ATestShould.java
@@ -0,0 +1,12 @@
+package io.bocchieri.codingexperience;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ATestShould {
+	@Test
+	void testJupiter() {
+		assertTrue(true);
+	}
+}
